4e25994fc1e67929a8920bddc7fabf582375843b
Madsen W. J. Servius <madsen@madsendynamics.com>
2022-09-27 18:39:04 -0400

    initial commit

---
+++ surveymng/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from surveymng.users.models import User
+from surveymng.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
