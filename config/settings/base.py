4a686ec2fa6a9b5c6c73ebed807d8774b80f5e38
Madsen W. J. Servius <madsen@madsendynamics.com>
2022-09-28 00:09:11 -0400

    login as home

--- config/settings/base.py
+++ config/settings/base.py
@@ -81,6 +81,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "surveymng.users",
+    "surveymng.main",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
