1ccd051f3f23dcafff6082561ca7ad6c0cdf1514
Madsen W. J. Servius <madsen@madsendynamics.com>
2022-09-29 06:28:35 -0400

    survey template

--- surveymng/main/views.py
+++ surveymng/main/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import redirect
 
 def index(request):
     return redirect("account_login")
+
+
+def create_agent(request):
+    pass
+
+
+def create_survey(request):
+    pass
+
+
+def create_formsurvey(request):
+    pass
